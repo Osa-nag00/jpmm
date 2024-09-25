a85fecc582462e07ee754c6c13c37babd03299aa
Osa-nag00 <naghiseosa2000@gmail.com>
2024-09-24 23:27:05 -0400

    made edit to run script to cover all version of jpmm

--- start-script/jpmm.bat
+++ start-script/jpmm.bat
@@ -1,2 +1,13 @@
-java -jar jpmm.jar
-pause
+@echo off
+setlocal
+
+rem Set the directory where the JAR file is located
+set "jarDirectory=%~dp0"
+
+rem Find the JAR file with the specified name pattern
+for %%F in ("%jarDirectory%\jpmm-*.jar") do (
+    echo Running %%F...
+    java -jar "%%F"
+)
+
+endlocal
